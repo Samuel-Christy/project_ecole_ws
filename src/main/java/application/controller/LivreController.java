045bac77f10585d8eeed9bf45328d9865d7c9b5b
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-11 14:12:04 +0200

    4nd

--- src/main/java/application/controller/LivreController.java
+++ src/main/java/application/controller/LivreController.java
@@ -9,25 +9,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.dao.LivreDAO;
 import application.models.Livre;
 
 @RestController
 public class LivreController {
 	
+	
 	@RequestMapping("ws/books")
+	//return List<Livre>
 	 public List<Livre> getLivres()
 	 {
-		 //@Request Select ALL BDD 
-		 List<Livre> list=new ArrayList<Livre>();
-		 list.add(new Livre());
-		 list.add(new Livre());
-		 list.add(new Livre());
-		 list.add(new Livre());
-		 list.add(new Livre());
-
-		 
-		 //return List<Livre>
-		 return list;
+		LivreDAO dao=new LivreDAO();
+		 return dao.getAllLivres();
 	 }
 
 	@GetMapping(path="ws/books/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
