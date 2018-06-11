224e9d2d666e620c42242114cfb4ccb07df12c2a
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-11 11:50:23 +0200

    Complete method on livreController

--- src/main/java/application/controller/LivreController.java
+++ src/main/java/application/controller/LivreController.java
@@ -1,11 +1,14 @@
 package application.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import application.models.Livre;
 
 @RestController
 public class LivreController {
@@ -14,15 +17,22 @@ public class LivreController {
 	 public List<Livre> getLivres()
 	 {
 		 //@Request Select ALL BDD 
+		 List<Livre> list=new ArrayList<Livre>();
+		 list.add(new Livre());
+		 list.add(new Livre());
+		 list.add(new Livre());
+		 list.add(new Livre());
+		 list.add(new Livre());
+
 		 
 		 //return List<Livre>
-		 return getLivre();
+		 return getLivres();
 	 }
 
-	 @RequestMapping(path="/books/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-	 public Livre getOccupation(@PathVariable("id")int n) {
+	@RequestMapping(path="ws/books/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	 public Livre getLivre(@PathVariable("id")int n) {
 	 
 			//@request INSERT (id,titre,annee,prenomAuteur,nomAuteur,editeur)
-		 return getLivre().get(n);
+		 return getLivres().get(n);
 	 }
 }
