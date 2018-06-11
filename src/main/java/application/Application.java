072bbb9a18cee826c65cefba5f8b65d1d36c1f16
Sam <s4perlipopette@gmail.com>
2018-06-11 12:02:02 +0200

    project setup for beans, application.properties, viewresolver

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,15 +1,36 @@
 package application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	@Value("${views:'erreur'}") // key:'defaultValue'
+	private String viewsPrefix;
 
+	public static void main(String[] args) {
+		AbstractApplicationContext ctxt = new FileSystemXmlApplicationContext("classpath:spring.xml");
 		SpringApplication.run(Application.class, args);
 
+	}
+
+	@Bean
+	public UrlBasedViewResolver getViewResolver() {
+
+		// System.out.println(viewsPrefix);
+		UrlBasedViewResolver v = new InternalResourceViewResolver();
+		v.setPrefix(viewsPrefix);
+		v.setSuffix(".jsp");
+		v.setViewClass(JstlView.class);
+		return v;
 	}
 
 }
---
+++ src/main/java/application/dao/LivreDAO.java
@@ -0,0 +1,27 @@
+package application.dao;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class LivreDAO {
+
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	public NamedParameterJdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(NamedParameterJdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public void init() {
+
+		String createDB = "CREATE TABLE IF NOT EXISTS livres ( `id` INTEGER PRIMARY KEY, `titre` TEXT, `annee` TEXT, `nom_auteur` TEXT, `prenom_auteur` TEXT, `editeur` TEXT);";
+		jdbcTemplate.getJdbcOperations().execute("DROP TABLE IF EXISTS etat;");
+		jdbcTemplate.getJdbcOperations().execute(createDB);
+
+		// feed the database :
+
+	}
+
+}
