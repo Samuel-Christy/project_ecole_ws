13e40f1dbf67c5838a88a14dc3ac834981e8c08d
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-11 11:39:33 +0200

    add RequestMapping on Controller

--- src/main/java/application/controller/LivreController.java
+++ src/main/java/application/controller/LivreController.java
@@ -1,5 +1,28 @@
 package application.controller;
 
-public class LivreController {
+import java.util.List;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LivreController {
+	
+	@RequestMapping("ws/book")
+	 public List<Livre> getLivres()
+	 {
+		 //@Request Select ALL BDD 
+		 
+		 //return List<Livre>
+		 return getLivre();
+	 }
+
+	 @RequestMapping(path="/books/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	 public Livre getOccupation(@PathVariable("id")int n) {
+	 
+			//@request INSERT (id,titre,annee,prenomAuteur,nomAuteur,editeur)
+		 return getLivre().get(n);
+	 }
 }
