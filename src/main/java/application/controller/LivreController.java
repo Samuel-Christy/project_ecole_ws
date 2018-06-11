cb007a707611e94e4266657e14ac16aba3655dfb
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-11 15:34:33 +0200

    add Attribute id

--- src/main/java/application/controller/LivreController.java
+++ src/main/java/application/controller/LivreController.java
@@ -33,12 +33,14 @@ public class LivreController {
 	public List<Livre> getLivres(Model model) {
 
 		ArrayList<Livre> livres = (ArrayList<Livre>) livreDAO.getAllLivres();
-		for (int i = 0; i < livres.size(); i++) {			
+		for (int i = 0; i < livres.size(); i++) {	
+		model.addAttribute("id",livres.get(i).getId());
 		model.addAttribute("titre", livres.get(i).getTitre());
 		model.addAttribute("annee", livres.get(i).getAnnee());
 		model.addAttribute("nom_auteur", livres.get(i).getNom_auteur());
 		model.addAttribute("prenom_auteur", livres.get(i).getPrenom_auteur());
 		model.addAttribute("editeur", livres.get(i).getEditeur());
+		
 		}
 		return livres;
 
