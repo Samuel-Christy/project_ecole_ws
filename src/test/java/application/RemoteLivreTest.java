a8e10a349439aa8528f092cdc158f1aaa66c76c1
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-12 15:57:36 +0200

    add comment to RemoteLivreTest

--- src/test/java/application/RemoteLivreTest.java
+++ src/test/java/application/RemoteLivreTest.java
@@ -9,6 +9,13 @@ import org.junit.Test;
 import application.models.RemoteLivre;
 
 public class RemoteLivreTest {
+	
+	/**
+	 * 
+	 * @result TitreLivre will be persisted without any errors,
+	 *         and livre.getTitre_livre() will no longer be <code>null</code>
+	 */
+	
 	@Test
 	public void testGetTitreLivre() {
 		 RemoteLivre livre = new RemoteLivre();
@@ -18,15 +25,25 @@ public class RemoteLivreTest {
 		
 	}
 	
+	/**
+	 * 
+	 * @result AnneeLivre will be persisted without any errors,
+	 *         and livre.getAnnee_livre() will no longer be <code>null</code>
+	 */
 	@Test
 	public void testGetAnneeLivre() {
 		RemoteLivre livre = new RemoteLivre();
 		String annee = "1947";
 		livre.setAnnee_livre(annee);
 		assertEquals("1947", livre.getAnnee_livre());
-		livre.setAnnee_livre(annee);
-		    
-}
+		livre.setAnnee_livre(annee);    
+		}
+	
+	/**
+	 * 
+	 * @result Nom_Auteur will be persisted without any errors,
+	 *         and livre.getNom_auteur will no longer be <code>null</code>
+	 */
 	@Test
 	public void testGetNom_Auteur() {
 		RemoteLivre livre = new RemoteLivre();
@@ -36,6 +53,11 @@ public class RemoteLivreTest {
 		livre.setNom_auteur("nom_auteur");
 }
 	
+	/**
+	 * 
+	 * @result EditeurLivre will be persisted without any errors,
+	 *         and livre.getEditeur_livre() will no longer be <code>null</code>
+	 */
 	@Test
 	public void testGetEditeurLivre() {
 		RemoteLivre livre = new RemoteLivre();
