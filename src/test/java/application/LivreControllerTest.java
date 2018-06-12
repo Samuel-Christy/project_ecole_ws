0692662f2799f97f7bc138f92baff8fac4ac3c04
Guillaume <guillaume.pozin@hotmail.fr>
2018-06-12 15:38:21 +0200

    complete livreControllerTest

--- src/test/java/application/LivreControllerTest.java
+++ src/test/java/application/LivreControllerTest.java
@@ -1,5 +1,17 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.models.Livre;
+
 public class LivreControllerTest {
+	
+	public List<Livre> getLivresTest() {
+		// mapped to hostname:/ws/books
+		ArrayList<Livre> livres = new ArrayList<Livre>;
+
+		return livres;
+	}
 
 }
